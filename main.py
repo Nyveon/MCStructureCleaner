4c515f4b32c394d26945b6434bd92f530da88ee7
Nyveon <54124297+Nyveon@users.noreply.github.com>
2021-01-24 21:49:40 -0300

    V1.1
    Better and more detailed output information.

--- main.py
+++ main.py
@@ -2,27 +2,31 @@
 MC Structure cleaner
 By: Nyveon
 
-v: 1.0
+v: 1.1
 
 Modded structure cleaner for minecraft. Removes all references to non-existent structures to allow for
 clean error logs and chunk saving.
 '''
 
 # Imports
-import anvil # anvil-parser by matcool
-import os
-version = "1.0"
+import anvil    # anvil-parser by matcool
+import os       # for file-system
+import time     # for progress messages
+version = "1.1"
+
+def sep():
+    print("----------------------------------")
+
 
 # User input
-print("----------------------------------")
+sep()
 print("MC Structure cleaner\nBy: Nyveon")
 print("Version: ", version)
-print("----------------------------------")
+sep()
 print("Please type in the EXACT structure tag name you want removed (Use NBTExplorer to find the name)")
-print("Tag:")
-to_replace = input() #todo: allow multiple tags
+to_replace = input("Tag: ") #todo: allow multiple tags
 print("Replacing", to_replace, "in all region files.")
-print("----------------------------------")
+sep()
 
 # Create new directory and access regions directory
 print("Saving newly generated region files to .\\new_region\\")
@@ -33,18 +37,23 @@ except:
 ld = os.listdir("./world/region") #todo: add other worlds/dimensions
 ldn = len(ld)
 print("Region files to process:", ldn)
+sep()
 counter = 1
 removed = 0
+previous_removed = 0
+last_time = time.time()
+
 
 # Main parsing loop
 for file_name in ld:
 
     # Current region file
     # todo: allow for picking up progress where it left off in case of termination
-    print("Progress: [", counter, "/", ldn,  "] | Processing:", file_name)
+    print("Progress: [", counter, "/", ldn,  "] | Processing:", file_name, "| ", end = "")
     coords = file_name.split(".")
     region = anvil.Region.from_file("./world/region/" + file_name)
     new_region = anvil.EmptyRegion(int(coords[1]), int(coords[2]))
+    previous_removed = removed
 
     # Check all possible chunks in the region (32 x 32 area)
     for chunk_x in range(32):
@@ -71,8 +80,11 @@ for file_name in ld:
     # Save generated region file and increment counter
     new_region.save("./new_region/" + file_name)
     counter += 1
+    delta_seconds = round((time.time() - last_time) % 60, 2)
+    last_time = time.time()
+    print(removed - previous_removed, "instances of the tag removed. |", delta_seconds, "seconds taken.")
 
-print("Done. Removed", removed, "instances of", to_replace, "in", ldn, "region files.")
+print("\nDone. Removed", removed, "instances of", to_replace, "in", ldn, "region files.")
 print("You can now replace the region folder in your world for the new_region folder.")
 
 
